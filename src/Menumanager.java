26efb02f7aa5cf65565c42906d0cb8487607f308
okguidong <ok1031@gnu.ac.kr>
2022-03-28 18:26:20 +0900

    mathod

--- src/Menumanager.java
+++ src/Menumanager.java
@@ -13,21 +13,40 @@ public class Menumanager {
 			System.out.println("4. View Credits");//학점 보기
 			System.out.println("5. Show menu");//메뉴보기
 			System.out.println("6. Exit");//나가기
-			num = input.nextInt();
-			switch(num) {
-			case 1:
-				System.out.print("subject name:");//"과목이름: "출력
-				String SubjectName=input.next();//과목이름 입력
-				System.out.print("credit score:\n ex) 4.5,4,3.5,2.5,2,1.5,1,0.5,0");//"과목 점수:" 출력
-				float CreditScore=input.nextFloat(); //과목점수 입력
-				System.out.print("credit:");//"이수학점:" 출력
-				int Credit=input.nextInt();//이수학점 입력
-			case 2:
-			case 3:
-			case 4:
-			case 5:
+			num = input.nextInt();//num 입력
+			if(num==1) {
+				addcredits();
+			}
+			else if(num==2) {
+				deletecredits();
+			}
+			else if(num==3) {
+				editcredits();
+			}
+			else if(num==4) {
+				viewcredits();
 			}
 		}
 	}
-
+	
+	public static void addcredits() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("subject name:");//"과목이름: "출력
+		String SubjectName=input.next();//과목이름 입력
+		System.out.print("credit score:\n ex) 4.5,4,3.5,2.5,2,1.5,1,0.5,0");//"과목 점수:" 출력
+		float CreditScore=input.nextFloat(); //과목점수 입력
+		System.out.print("credit:");//"이수학점:" 출력
+		int Credit=input.nextInt();//이수학점 입력	
+		
+	}
+	public static void deletecredits() {
+		
+	}
+	public static void editcredits() {
+		
+	}
+	public static void viewcredits() {
+		
+	}
+	
 }
