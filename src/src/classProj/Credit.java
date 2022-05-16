d49177ace45939c3cda1d5d89105dc2f30314305
okguidong <101470646+okguidong@users.noreply.github.com>
2022-05-16 21:09:47 +0900

    assignment #8
    assignment #8

---
+++ src/src/classProj/Credit.java
@@ -0,0 +1,54 @@
+package classProj;
+
+public abstract class Credit {
+	private String name;
+	private int credit;
+	private float score;
+	
+	public void setname(String name) 
+	{
+		this.name = name;
+	}
+	
+	public void setcredit(int credit) 
+	{
+		this.credit = credit;
+	}
+	
+	public void setscore(float score) 
+	{
+		this.score = score;
+	}
+	
+	public int getcredit() {
+		return this.credit;
+	}
+	
+	public float getscore() {
+		return this.score;
+	}
+	
+	public String getname() {
+		return this.name;
+	}
+	abstract void print();
+	
+	public float averageUp(String a) {
+		if(a=="전체") {
+			return getscore()*getcredit();
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public float averageDown(String a) {
+		if(a=="전체") {
+			return getcredit();
+		}
+		else {
+			return 0;
+		}
+	}
+}
+
---
+++ src/src/classProj/Kind.java
@@ -0,0 +1,66 @@
+package classProj;
+
+public class Kind extends Credit implements Calculator{
+	private String kind;
+	
+	public void setkind(String kind) 
+	{
+		this.kind = kind;
+	}
+	
+	public String getkind() 
+	{
+		return this.kind;
+	}
+	
+	public void print()
+	{
+		System.out.println(getname() + " / " + getscore() + " / " + getcredit() + " / " + getkind());	
+	}
+	public float averageUp(String a) {
+		float x=0;
+
+		if(a=="교양")
+		{
+			if(getkind().equals("교양")) {//교양 평균
+				x= getscore()* getcredit();
+
+			}
+		}
+		else if(a=="전공") //전공의 평균
+		{
+			if(getkind().equals("전공")){
+				x= getscore() * getcredit();
+			}
+		}
+
+		else
+		{//전체의 편균	
+			x= getscore() * getcredit();
+		}
+		return x;
+	}
+	
+	public float averageDown(String a) {
+		float x=0;
+
+		if(a=="교양")
+		{
+			if(getkind().equals("교양")) {//교양 평균
+				x= getcredit();
+			}
+		}
+		else if(a=="전공") //전공의 평균
+		{
+			if(getkind().equals("전공")){
+				x= getcredit();
+			}
+		}
+
+		else
+		{//전체의 평균	
+			x= getcredit();
+		}
+		return x;
+	}
+}
